a0d92b208a7ba49abc019bb9b435d4b4d45043a1
Matua Doc <88599935+MatuaDoc@users.noreply.github.com>
2024-06-10 20:50:06 +0000

    Jun 10, 2024, 8:50 PM

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-interface Person {  //A
-  firstName: string; //B
-  lastName: string; //C
-}
+import readline from 'readline';
 
-function greeter(person: Person) { //D
-  return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-let user = { firstName: "Joe", lastName: "Kim" };
-
-document.body.innerText = greeter(user);
